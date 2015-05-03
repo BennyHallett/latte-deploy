e6c77e3e2a023c4986a0e3fc53f0d7d134eb6615
BennyHallett <benny.hallett@gmail.com>
2015-05-03 11:57:55 +1000

    added messaging partial and styling. Added sidebar

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,9 @@
 class DashboardController < ApplicationController
 
   def index
+    flash[:info] = "You haven't created a project yet." unless Project.count > 0
+
+    @projects = Project.all
   end
 
 end
--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -1,7 +1,52 @@
 require 'test_helper'
 
 class DashboardControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'should show message when no projects exist' do
+    get :index
+
+    assert_response :success
+    assert_select '.messages', 1
+    assert_select '.messages' do
+      assert_select '.alert-info' do
+        assert_select 'strong', "You haven't created a project yet."
+      end
+    end
+  end
+
+  test 'should not show message when project exists' do
+    project = FactoryGirl.build(:project)
+    project.save
+
+    get :index
+
+    assert_response :success
+    assert_select '.messages', 0
+  end
+
+  test 'should show project and repos when one exists' do
+    project = FactoryGirl.build(:project)
+    repo1 = FactoryGirl.build(:repo, name: 'repo_one')
+    repo2 = FactoryGirl.build(:repo, name: 'repo_two')
+    project.repos << repo1
+    project.repos << repo2
+    repo1.save
+    repo2.save
+    project.save
+
+    get :index
+
+    assert_response :success
+    assert_select '.projects' do
+      assert_select '.project', 1
+      assert_select '.project' do
+        assert_select 'h3', project.name
+
+        assert_select '.repo', 2
+        assert_select 'h4', repo1.name
+        assert_select 'h4', repo2.name
+      end
+    end
+  end
+
 end
