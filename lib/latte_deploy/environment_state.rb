2cc6d94996d9c67a58ebdf3dcd9d7268c393d9a0
BennyHallett <benny.hallett@gmail.com>
2015-05-20 08:23:17 +1000

    Fixed displaying of environment state

--- lib/latte_deploy/environment_state.rb
+++ lib/latte_deploy/environment_state.rb
@@ -2,12 +2,21 @@ class LatteDeploy::EnvironmentState
 
   attr_reader :status, :text
 
+  @@failure_cases = %w{fail failed failure error bad}
+  @@success_cases = %w{success ok successful good}
+
   def self.unknown
     new('info', 'Unknown.')
   end
 
   def initialize(status, text)
-    @status = status
+    if @@failure_cases.include? status
+      @status = 'danger'
+    elsif @@success_cases.include? status
+      @status = 'success'
+    end
+
+    @status ||= status
     @text = text
   end
 
